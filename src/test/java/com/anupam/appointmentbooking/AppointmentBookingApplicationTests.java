aafc05945f2e06e7281df9d67ab9c2efe831e817
AwsomeAnupam <anupam140587@gmail.com>
2024-09-17 18:02:09 +0530

    Add files via upload

---
+++ src/test/java/com/anupam/appointmentbooking/AppointmentBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anupam.appointmentbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
